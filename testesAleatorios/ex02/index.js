959a06e7c1a97f734a57ef40f2e30da48b3da28e
Joseph-Zeud <161529692+Joseph-Zeud@users.noreply.github.com>
2024-07-02 16:56:15 -0300

    teste logico

--- testesAleatorios/ex02/index.js
+++ testesAleatorios/ex02/index.js
@@ -1,11 +1,14 @@
 let ent1 = 0
 function media (){
-let num1txt = window.document.getElementById("inp1").value;
+let num1txt = document.getElementById("inp1").value;
 let num2txt = document.getElementById('inp2').value;
 let num1 = parseFloat(num1txt)
 let num2 = parseFloat(num2txt)
-let soma =( num1+num2)/2
-document.getElementById('resultado').innerHTML = soma
+let soma =(( num1+num2)/2).toFixed(2)
+document.getElementById('resultado').innerHTML =  `Sua media foi ${soma}` 
+document.getElementById('inp1').value=('')
+document.getElementById('inp2').value=('')
+
 
 
 
---
+++ testesAleatorios/multiplos/index.js
@@ -0,0 +1,7 @@
+function teste(){
+ inpt = window.document.getElementById("inp1").value;
+ let num1 = parseInt(inpt)
+   
+  }
+
+    
